e9afb04e66e12a70322bd1c93d4aafc862628f52
Navneet Pandey <npandey057@gmail.com>
2016-05-06 15:37:57 +0530

    add css and header of the app view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup',
           via: [:get, :patch], :as => :finish_signup
+
+  root to: "homes#index"
 end
