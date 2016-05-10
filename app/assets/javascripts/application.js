66b2e372b8e3f28560824849389de6ea621520e7
Navneet Pandey <npandey057@gmail.com>
2016-05-10 18:32:02 +0530

    remove cdn and add in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery.min
 //= require turbolinks
 //= require jquery_ujs
+//= require imagesloaded.min
+//= require masonry.min
 //= require_tree .
